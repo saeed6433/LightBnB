4da04344592a3c198182814639e5a7ed9a8ff690
Saeed <saeed6433@gmail.com>
2023-09-22 23:56:16 -0400

    Refactor users functions

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,15 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
+//const properties = require("./json/properties.json");
+//const users = require("./json/users.json");
+
+const { Pool } = require('pg');
+
+//establishing connection
+const pool = new Pool({
+  user: 'saeedjeiroodi',
+  password: 'labber',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
 
@@ -8,15 +18,14 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+const getUserWithEmail = function (email) {    
+    return pool
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {console.error(err.message);
+    });
 };
 
 /**
@@ -25,7 +34,14 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((res) => {      
+      return res.rows[0];
+    })
+    .catch((err) => {console.error(err.message);
+    });
+  
 };
 
 /**
@@ -34,10 +50,16 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(
+      `INSERT INTO  users (name, email, password) VALUES ($1, $2, $3) RETURNING *`,
+      [user.name, user.email, user.password]
+    )
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {console.error(err.message);
+    });
 };
 
 /// Reservations
