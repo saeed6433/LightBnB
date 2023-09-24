14c665bb8a30bb27428ccda7fd1d83ecbffde13d
Saeed <saeed6433@gmail.com>
2023-09-24 18:15:00 -0400

    Get all reservation

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -70,7 +70,20 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
+const queryParams = [guest_id, limit];
+return pool
+  .query(queryString, queryParams)
+  .then((res) => res.rows) 
+  .catch((err) => console.log(err.message));
 };
 
 /// Properties
